a4dbd6434dd0a59633dd6988be9070a218db6640
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2025-03-10 23:16:47 +1100

    planned out language / tool placeholder.

--- src/assets/Components/ProgrammingLanguages.tsx
+++ src/assets/Components/ProgrammingLanguages.tsx
@@ -3,18 +3,23 @@ import { knownLanguagesToMap } from "./ProgrammingLanguagesHelper";
 
 const ProgrammingLanguages = () => {
   return (
-    <div className="flex w-full flex-row">
-      {knownLanguagesToMap.map((language) => {
-        return (
-          <a href="https://react.dev" target="_blank">
-            <img
-              src={language.logo}
-              alt={language.alt}
-              className={`logo ${language.additionalClassname}`}
-            />
-          </a>
-        );
-      })}
+    <div className="flex flex-col h-fit">
+      <div className="py-5">
+        <h1>Languages / Tools</h1>
+      </div>
+      <div className="flex w-full flex-row h-fit gap-10">
+        {knownLanguagesToMap.map((language) => {
+          return (
+            <div className="bg-[#191818] w-24 h-24 flx justify-center items-center logo">
+              <img
+                src={language.logo}
+                alt={language.alt}
+                className={`rounded-xl border-rounded p-2 logo ${language.additionalClassname}`}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
