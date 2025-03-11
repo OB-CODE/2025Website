64a74a9d0e1334b1f696bd352213487a0092cf8d
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2025-03-11 19:22:01 +1100

    Added all languages. Changed styling within ProgrammingLanguages.

--- src/assets/Components/ProgrammingLanguages.tsx
+++ src/assets/Components/ProgrammingLanguages.tsx
@@ -1,24 +1,25 @@
-import React from "react";
 import { knownLanguagesToMap } from "./ProgrammingLanguagesHelper";
 
 const ProgrammingLanguages = () => {
   return (
     <div className="flex flex-col h-fit">
       <div className="py-5">
-        <h1>Languages / Tools</h1>
+        <h1>Languages + Tools</h1>
       </div>
-      <div className="flex w-full flex-row h-fit gap-10">
-        {knownLanguagesToMap.map((language) => {
-          return (
-            <div className="bg-[#191818] w-24 h-24 flx justify-center items-center logo">
-              <img
-                src={language.logo}
-                alt={language.alt}
-                className={`rounded-xl border-rounded p-2 logo ${language.additionalClassname}`}
-              />
-            </div>
-          );
-        })}
+      <div className="flex w-full justify-center">
+        <div className="w-[80%] flex flex-row flex-wrap gap-10">
+          {knownLanguagesToMap.map((language) => {
+            return (
+              <div className="w-24 h-24 flx justify-center items-center logo">
+                <img
+                  src={language.logo}
+                  alt={language.alt}
+                  className={`rounded-xl border-rounded p-2 logo ${language.additionalClassname}`}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/assets/Components/ProgrammingLanguagesHelper.tsx
+++ src/assets/Components/ProgrammingLanguagesHelper.tsx
@@ -5,21 +5,59 @@ interface ILanguages {
   language: string;
   logo: string | undefined;
   alt: string;
-  additionalClassname: string | null; // used to add specific classnames to mapping.
+  additionalClassname?: string; // used to add specific classnames to mapping.
 }
 
 export const knownLanguagesToMap: ILanguages[] = [
+  {
+    language: "JavaScript",
+    logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    alt: "JavaScript Logo",
+  },
+  {
+    language: "NodeJS",
+    logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+    alt: "Node JS Logo",
+  },
+  {
+    language: "HTML5",
+    logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    alt: "HTML5 Logo",
+  },
   {
     language: "React",
     logo: reactLogo,
     alt: "React Logo",
     additionalClassname: "logoRotate",
   },
-  //   { language: "Vite", logo: viteLogo },
+  {
+    language: "Jest",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg",
+    alt: "Jest Logo",
+  },
+  {
+    language: "Postman",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg",
+    alt: "Postman Logo",
+  },
+  {
+    language: "TypeScript",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+    alt: "TypeScript Logo",
+  },
+  {
+    language: "Next.JS",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+    alt: "Next.JS Logo",
+  },
   {
     language: "Tailwind",
-    logo: "tst",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original-wordmark.svg",
     alt: "tailwind Logo",
-    additionalClassname: null,
+  },
+  {
+    language: "C#",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg",
+    alt: "C# Logo",
   },
 ];
