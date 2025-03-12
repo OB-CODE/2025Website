16640d7bee946b1397645ba8eade96d1cc255ff9
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2025-03-12 19:13:44 +1100

    Updates to Aurora Text for name on webpage.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-[#151515] w-screen p-0 m-0 h-screen text-white text-center flex items-center flex-col justify-center">
+    <div className="bg-[black] w-screen p-0 m-0 h-screen text-white text-center flex items-center flex-col justify-center">
       <PersonalHeading />
       <div>
         <ProgrammingLanguages />
--- src/components/PersonalHeading/PersonalHeading.tsx
+++ src/components/PersonalHeading/PersonalHeading.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { personalDetails } from "./personal-info";
 import { HyperText } from "../magicui/hyper-text";
+import { AuroraText } from "../magicui/aurora-text";
 
 const PersonalHeading = () => {
   const [toggleForRightBorder, setToggleForRightBorder] = useState(true);
@@ -14,16 +15,35 @@ const PersonalHeading = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className="py-2">
-        {personalDetails.firstName} {personalDetails.lastName}
-      </h1>
+    <div className="flex flex-col justify-center items-center h-full w-full bg-[radial-gradient(ellipse_at_center,_rgba(60,60,60,0.7)_0%,_rgba(0,0,0,1)_60%)]">
+      <div className="flex flex-row w-fit justify-between items-center">
+        <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
+          {personalDetails.firstName}{" "}
+          <AuroraText
+            speed={2}
+            // colors={[
+            //   "#ffffff",
+            //   "#e0c3fc",
+            //   "#c084fc",
+            //   "#9333ea",
+            //   "#7e22ce",
+            //   "#6b21a8",
+            // ]}
+          >
+            {personalDetails.lastName}
+          </AuroraText>
+        </h1>
+        <div className="w-3 pl-1 h-full flex items-center">
+          <div
+            className={`z-10 flex h-[60%] w-1 ${
+              toggleForRightBorder ? "border-r" : "border-transparent"
+            } `}></div>
+        </div>
+      </div>
       <div
-        className={`bg-[rgba(186,58,249,0.17)] w-fit h-16 flex justify-center opacity px-2 items-center ${
-          toggleForRightBorder ? "border-r" : ""
-        }`}>
+        className={`z-10 bg-[rgba(168,85,247,0.2)] w-fit h-16 flex justify-center rounded-4xl opacity px-2 items-center `}>
         <div className="text-5xl bottom-1 relative">{`<`}</div>
-        <HyperText duration={400} characterSet={["0", "1"]}>
+        <HyperText duration={300} characterSet={["0", "1"]}>
           {personalDetails.jobTitle}
         </HyperText>
         <div className="text-5xl bottom-1 relative pl-2">{`/>`}</div>
