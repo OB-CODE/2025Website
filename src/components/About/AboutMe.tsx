2c6bde74878db9379e2d5c6ac790cbf4323bebf0
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2025-03-23 19:10:19 +1100

    About me section updated.

--- src/components/About/AboutMe.tsx
+++ src/components/About/AboutMe.tsx
@@ -1,20 +1,22 @@
 import Heading2 from "../ui/heading2";
 
 const aboutMeBlurb = {
-  1: "I'm Mitchell O'Brien. I am a Software Engineer and I have a passion for problem solving and working with others.",
+  1: "I have a passion for problem solving and working with others.",
   2: "When im not wearing my coding hat, I can be found chilling my my partner and dog, surrfing, working out, bouldering, eating and drinking too much coffee.",
+  3: "It's often said a good tradesman never blames their tools - That's why I decided to tool with a split Dvorak layout. It's a lot more believable to blame the tools when they look more complicated ;)",
 };
 
 const AboutMe = () => {
   return (
-    <div className="w-[80%] flex justify-center flex-col pb-10">
+    <div className="w-[80%] flex justify-center flex-col pb-5">
       <div className="w-full flex justify-end">
         <Heading2 inputString="About Me" />
       </div>
       <div className="flex flex-col items-end text-right w-full pt-6">
-        <div className="flex flex-col w-[50%] gap-3">
-          <div>{aboutMeBlurb[1]}</div>
-          <div>{aboutMeBlurb[2]}</div>
+        <div className="flex flex-col w-[70%] gap-3 items-end">
+          <div className="">{aboutMeBlurb[1]}</div>
+          <div className=" text-gray-400">{aboutMeBlurb[2]}</div>
+          <div className="text-gray-400">{aboutMeBlurb[3]}</div>
         </div>
       </div>
     </div>
