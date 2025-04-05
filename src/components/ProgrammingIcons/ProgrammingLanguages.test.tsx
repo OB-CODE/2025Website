df525a9e0c6216dacd9f1d716ae1bfec1e87d98d
OB-CODE <mitchell.s.obrien@gmail.com>
2025-04-06 08:20:04 +1000

    Mock renderToString in ProgrammingLanguages tests to prevent "MessageChannel is not defined" error.
    Node environment was not able to run the browser animation.

--- src/components/ProgrammingIcons/ProgrammingLanguages.test.tsx
+++ src/components/ProgrammingIcons/ProgrammingLanguages.test.tsx
@@ -3,6 +3,11 @@ import "@testing-library/jest-dom";
 import ProgrammingLanguages from "./ProgrammingLanguages";
 import { knownLanguagesToMap } from "./ProgrammingLanguagesHelper";
 
+// Mock renderToString to avoid "MessageChannel is not defined" error
+jest.mock("react-dom/server", () => ({
+  renderToString: jest.fn(() => "<div>Mocked Render</div>"),
+}));
+
 describe("ProgrammingLanguages component", () => {
   test("should render a div for each language in the knownLanguagesToMap array", () => {
     const expectedDivsRendered = knownLanguagesToMap.length;
