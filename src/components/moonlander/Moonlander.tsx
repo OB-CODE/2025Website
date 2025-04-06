e200214a1e5b55976103df8d0eec6e939b623f6f
OB-CODE <mitchell.s.obrien@gmail.com>
2025-04-06 13:06:49 +1000

    Responsive design adjustment - Increased Moonlander height on larger screens.

--- src/components/moonlander/Moonlander.tsx
+++ src/components/moonlander/Moonlander.tsx
@@ -3,7 +3,7 @@ import { BorderBeam } from "../magicui/border-beam";
 const Moonlander = () => {
   return (
     <div className=" w-[80%] overflow-hidden">
-      <div className="relative min-h-[555px] xl:min-h-[750px] bg-black w-full overflow-hidden">
+      <div className="relative min-h-[555px] xl:min-h-[800px] bg-black w-full overflow-hidden">
         <iframe
           src="https://configure.zsa.io/embed/moonlander/layouts/gdxrX/latest/0?theme=dark"
           className="absolute top-0 left-0 w-full h-full border-0 bg-black"
@@ -12,7 +12,8 @@ const Moonlander = () => {
             filter: "invert(1) hue-rotate(180deg)",
             height: "100%",
             overflow: "hidden", // Try setting the iframe's own overflow
-          }}></iframe>
+          }}
+        ></iframe>
         <BorderBeam duration={6} size={200} />
       </div>
     </div>
