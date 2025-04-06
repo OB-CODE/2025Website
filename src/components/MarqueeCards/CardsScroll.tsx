84cdaf6c2e745d069840177674fd88369ca6ef31
OB-CODE <mitchell.s.obrien@gmail.com>
2025-04-06 13:05:24 +1000

    addEventListener for screen size to increase the cards to map on extra large screen. External lib lop feature not working so solved via dynamically adjusting to mapped content.

--- src/components/MarqueeCards/CardsScroll.tsx
+++ src/components/MarqueeCards/CardsScroll.tsx
@@ -1,18 +1,42 @@
 import { cn } from "../../lib/utils";
 import Marquee from "react-fast-marquee";
 import { objsToMap } from "./cardsToMap";
+import { useEffect, useState } from "react";
 
 const CardsScroll = ({ direction }: { direction: "left" | "right" }) => {
+  const [dynamicObjectsToMap, setDynamicObjectsToMap] = useState(objsToMap); // Default loop count
+
+  // Adjust the loop count based on screen size
+  useEffect(() => {
+    const increaseNumberOfObjectsToMap = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth > 1920) {
+        setDynamicObjectsToMap([...objsToMap, ...objsToMap, ...objsToMap]); // More loops for extra-large screens
+      } else if (screenWidth > 1280) {
+        setDynamicObjectsToMap([...objsToMap, ...objsToMap]); // Medium-large screens
+      } else {
+        setDynamicObjectsToMap([...objsToMap]); // Default for smaller screens
+      }
+    };
+
+    increaseNumberOfObjectsToMap(); // Set initial loop count
+    window.addEventListener("resize", increaseNumberOfObjectsToMap); // Update on resize
+
+    return () => {
+      window.removeEventListener("resize", increaseNumberOfObjectsToMap); // Cleanup
+    };
+  }, []);
+
   return (
     <Marquee
       pauseOnHover={true}
-      loop={3}
+      loop={5}
       gradient={true}
       gradientColor={"black"}
       direction={direction}
       className="max-w-[calc(100vw)] md:max-w-[calc(100vw-30px)] flex justify-between group  overflow-hidden p-0 [gap:var(--gap)] flex-row space-x-1 "
     >
-      {objsToMap.map((obj, index) => {
+      {dynamicObjectsToMap.map((obj, index) => {
         return (
           <figure
             className={cn(
