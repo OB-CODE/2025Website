9bd4e55165a971693798a3fb6e66045c179036cd
OB-CODE <mitchell.s.obrien@gmail.com>
2025-04-10 21:52:44 +1000

    Add EmailFrom component and update ModalCard to include email messaging

---
+++ lib/EmailFrom.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmailFrom = () => {
+  return (
+    <div>
+      <div> Or shoot me a message from here:</div>
+    </div>
+  );
+};
+
+export default EmailFrom;
--- lib/ModalCard.tsx
+++ lib/ModalCard.tsx
@@ -5,6 +5,7 @@ import {
   EnvelopeIcon,
   PhoneIcon,
 } from "@heroicons/react/24/outline";
+import EmailFrom from "./EmailFrom";
 
 const ModalCard = ({
   toggleContactPopup,
@@ -24,14 +25,17 @@ const ModalCard = ({
 
       <div
         id="messageContainer"
-        className="h-fit w-fit flex-col inset-0 z-30 bg-white text-black flex items-center justify-center rounded shadow-lg p-4"
+        className="h-fit w-fit flex-col inset-0 z-30 bg-black text-white border-2 border-white flex items-center justify-center rounded shadow-lg p-4"
       >
         <h2 className="text-xl font-bold mb-4">{modalContent.header}</h2>
 
-        <div className="flex gap-x-4 mb-4 w-full">
-          <div className="flex flex-col  w-[45%]" id="details">
+        <div className="flex justify-between gap-x-4 mb-4 w-full text-gray-400 ">
+          <div className="flex flex-col w-[50%]" id="details">
             {modalContent.persoanlInformation.map((line, index) => (
-              <div key={index} className="flex mb-2 items-center w-full gap-2">
+              <div
+                key={index}
+                className="flex mb-2 py-2 items-center w-full gap-2"
+              >
                 <div className="flex items-center gap-2">
                   {line.symbol && ( // Ensure symbol is rendered correctly
                     <span className="ml-2">{line.symbol}</span> // Adjusted to render symbol correctly
@@ -45,7 +49,7 @@ const ModalCard = ({
             ))}
           </div>
           <div className="flex flex-col w-[40%] px-8 text-left" id="blurb">
-            Shoot me a message on any of the platforms below:
+            <EmailFrom />
           </div>
         </div>
 
