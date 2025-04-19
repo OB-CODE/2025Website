f79581328b9b09319444f7bce5f28cd05ff9f786
OB-CODE <mitchell.s.obrien@gmail.com>
2025-04-19 20:56:17 +1000

    added a tech stack flip to each card.

--- src/components/projects/ProjectCards.tsx
+++ src/components/projects/ProjectCards.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { FaGlobe } from "react-icons/fa";
-
+import { TbStack } from "react-icons/tb";
 import GlowCard from "../ui/GlowCard";
 import { Tooltip } from "react-tooltip";
 
@@ -9,6 +10,7 @@ interface IprojectsToMap {
   mainImage?: string;
   github?: string;
   website?: string;
+  techStack?: string[];
 }
 
 const ProjectCards = () => {
@@ -18,7 +20,7 @@ const ProjectCards = () => {
       description: "The best drinks company ever? IYKYK.",
       mainImage: "/IndigoMainImage.png",
       website: "https://drinkindigo.com.au/",
-      // no Github - Private.
+      techStack: ["Liquid", "Shopify", "vanilla CSS", "JavaScript"],
     },
     {
       name: "Pokemon Remastered",
@@ -27,6 +29,14 @@ const ProjectCards = () => {
       mainImage: "/PokemonRemasteredMainImage.png",
       website: "https://poke-battles-remastered.vercel.app/",
       github: "https://github.com/OB-CODE/PokeBattlesRemastered",
+      techStack: [
+        "React",
+        "Node.js",
+        "Next.js",
+        "TypeScript",
+        "TailwindCSS",
+        "DynamoDB",
+      ],
     },
     {
       name: "Home Harvest",
@@ -35,101 +45,123 @@ const ProjectCards = () => {
       mainImage: "/HomeHarvestMainImage.png",
       website: "https://mitchell-home-harvest.surge.sh/",
       github: "https://github.com/OB-CODE/Home-Harvest",
+      techStack: ["React", "Node.js", "surge", "vanilla CSS"],
     },
-
     {
       name: "Original Website",
       description: "My original website - showcasing some older projects.",
       mainImage: "/OldWebsiteMainImage.png",
       website: "https://www.mitch-obrien.com/",
       github: "https://github.com/OB-CODE/Personal-Website",
+      techStack: ["React", "Node.js", "AWS Amplify"],
     },
   ];
 
+  const [hoveringTechStackIndex, setHoveringTechStackIndex] = useState<
+    number | null
+  >(null);
+
   return (
     <div className="w-full h-full flex flex-wrap justify-between">
-      {projectsToMap.map((project, index) => {
-        return (
-          <GlowCard
-            key={index}
-            glowSize={250}
-            glowColor="#444444"
-            className="w-[90%] md:w-[48%] m-1 mb-5 min-h-[50vh] flex flex-col"
-          >
-            {/* Header */}
-            <div data-testid="projectContainer" className="flex w-full ">
-              <div
-                data-testid="projectHeaderTray"
-                className="flex w-full justify-end p-1 px-3 pt-3"
-              >
-                {/* <div>x</div> */}
-                <div className="flex">
-                  <a href={project.website} target="_blank" rel="noreferrer">
-                    <div className="border hover:cursor-pointer hover:bg-blue-500 hover:border-blue-500 w-6 h-6 flex items-center justify-center bg-gray-400 rounded-4xl mx-1">
-                      <FaGlobe size={40} color="black" />
-                    </div>
-                  </a>
-
-                  <div
-                    className={`border ${
-                      project.github
-                        ? "hover:cursor-pointer hover:bg-blue-500 hover:border-blue-500 "
-                        : ""
-                    } w-6 h-6 flex items-center justify-center bg-gray-400 rounded-4xl mx-1`}
-                  >
-                    <a
-                      data-tooltip-id={project.github ? "" : "my-tooltip"}
-                      data-tooltip-content="This ones private - Sorry!"
-                      data-tooltip-place="top"
-                      href={project.github}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <img
-                        src={`https://cdn.simpleicons.org/github`}
-                        alt="githubLogo"
-                        height={40}
-                        width={40}
-                      />
-                    </a>
-                  </div>
+      {projectsToMap.map((project, index) => (
+        <GlowCard
+          key={index}
+          glowSize={250}
+          glowColor="#444444"
+          className="w-[90%] md:w-[48%] m-1 mb-5 min-h-[50vh] flex flex-col"
+        >
+          {/* Header */}
+          <div data-testid="projectContainer" className="flex w-full">
+            <div className="flex justify-between w-full">
+              <div className="p-1 pl-4 w-full flex justify-start items-center">
+                <div
+                  className="flex items-center"
+                  onMouseEnter={() => setHoveringTechStackIndex(index)} // Set the hovered card index
+                  onMouseLeave={() => setHoveringTechStackIndex(null)} // Reset the hovered card index
+                >
+                  <TbStack className="cursor-help" />
+                  <span className="text-gray-500 pl-2 cursor-help">
+                    Tech Stack
+                  </span>
                 </div>
               </div>
             </div>
-
-            {/* Body - Expands to fill remaining space */}
             <div
-              data-testid="projectBody"
-              className="min-h-0 flex items-center justify-center flex-grow-2"
+              data-testid="projectHeaderTray"
+              className="flex w-full justify-end p-1 px-3 pt-3"
             >
-              <div className="flex w-full h-full items-center justify-center">
-                {project.mainImage ? (
-                  <img
-                    src={project.mainImage}
-                    className="opacity-70 w-full max-w-[700px] max-h-[315px] p-4"
-                    alt=""
-                  />
-                ) : (
-                  "img"
-                )}
+              <div className="flex">
+                <a href={project.website} target="_blank" rel="noreferrer">
+                  <div className="border hover:cursor-pointer hover:bg-blue-500 hover:border-blue-500 w-6 h-6 flex items-center justify-center bg-gray-400 rounded-4xl mx-1">
+                    <FaGlobe size={40} color="black" />
+                  </div>
+                </a>
+
+                <div
+                  className={`border ${
+                    project.github
+                      ? "hover:cursor-pointer hover:bg-blue-500 hover:border-blue-500"
+                      : ""
+                  } w-6 h-6 flex items-center justify-center bg-gray-400 rounded-4xl mx-1`}
+                >
+                  <a
+                    data-tooltip-id={project.github ? "" : "my-tooltip"}
+                    data-tooltip-content="This one's private - Sorry!"
+                    data-tooltip-place="top"
+                    href={project.github}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={`https://cdn.simpleicons.org/github`}
+                      alt="githubLogo"
+                      height={40}
+                      width={40}
+                    />
+                  </a>
+                </div>
               </div>
             </div>
+          </div>
 
-            {/* Footer */}
-            <div
-              data-testid="projectFooter"
-              className="flex flex-col w-full items-center justify-start min-h-[12vh] pb-4"
-            >
-              <div className="w-[80%] text-left text-gray-500 text-xl">
-                {project.name}
-              </div>
-              <div className="w-[80%] text-left text-gray-200">
-                {project.description}
-              </div>
+          {/* Body - Expands to fill remaining space */}
+          <div
+            data-testid="projectBody"
+            className="min-h-0 flex items-center justify-center flex-grow-2"
+          >
+            <div className="w-full max-w-[700px] max-h-[315px] h-[315px] relative">
+              {hoveringTechStackIndex === index ? (
+                <div className="absolute inset-0 flex items-center justify-center text-white  p-4 flex-col">
+                  {project.techStack?.map((tech, techIndex) => (
+                    <div key={techIndex} className="text-gray-500">
+                      {tech}
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <img
+                  src={project.mainImage}
+                  className="absolute inset-0 w-full h-full object-cover opacity-70  p-4"
+                  alt=""
+                />
+              )}
             </div>
-          </GlowCard>
-        );
-      })}
+          </div>
+
+          {/* Footer */}
+          <div
+            data-testid="projectFooter"
+            className="flex flex-col w-full items-center justify-start min-h-[12vh] pb-4"
+          >
+            <div className="w-[80%] text-left text-gray-500 text-xl">
+              {project.name}
+            </div>
+            <div className="w-[80%] text-left text-gray-200">
+              {project.description}
+            </div>
+          </div>
+        </GlowCard>
+      ))}
       <Tooltip id="my-tooltip" />
     </div>
   );
