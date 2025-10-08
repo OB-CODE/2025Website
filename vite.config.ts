9002ed001a2d88929780f61e2181d3ad9b783855
Mitchell O'Brien <115781530+OB-CODE@users.noreply.github.com>
2025-10-08 19:24:02 +1100

    polyFill for global crypto

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import { webcrypto } from "crypto";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+
+// Polyfill for webcrypto
+if (!globalThis.crypto) {
+  (globalThis as any).crypto = webcrypto;
+}
 
 // https://vite.dev/config/
 export default defineConfig({
